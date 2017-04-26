a339bb501d412b9507e5cb5850dece66747a2b01
Esteban Dalel R <estebandalelr@gmail.com>
2017-04-25 21:33:55 -0500

    Allowed the webview to open links without an external browser and added comments to main activity

--- MyApplication/Hospitalpes/src/main/java/com/example/esteban/myapplication/MainActivity.java
+++ MyApplication/Hospitalpes/src/main/java/com/example/esteban/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Create Webview
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
+        //allow JS
         webSettings.setJavaScriptEnabled(true);
+        //URL to be loaded
         mWebView.loadUrl("http://estebandalelr.co/");
+        //force links in web view
+        mWebView.setWebViewClient(new WebViewClient());
     }
 }
